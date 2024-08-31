a1a23f8d211491db42fd62b4d982b76ff8ad4d93
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2024-08-31 15:03:44 +0530

    currency converter

--- react-10-projects/project-1/src/components/Navigation.jsx
+++ react-10-projects/project-1/src/components/Navigation.jsx
@@ -4,14 +4,14 @@ const Navigation = () => {
     <div>
         <nav className="container">
             <div className="logo">
-            <img src="/images/brand_logo.png" alt="logo" />
+                <img src="/images/brand_logo.png" alt="logo" />
             </div>
 
             <ul>
-            <li>Menu</li>
-            <li>Location</li>
-            <li>About</li>
-            <li>Contact</li>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
             </ul>
 
             <button>Login</button>
